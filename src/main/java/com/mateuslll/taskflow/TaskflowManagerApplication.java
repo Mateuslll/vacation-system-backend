e39240a78187b61cdff0c1bb2bd2814d542c5b5f
mateuslll <lima.mt96@gmail.com>
2026-04-28 18:57:21 +0100

    chore(backend): bootstrap spring project base
    Inicializa estrutura base do projeto com Maven wrapper, entrypoint e configuração inicial da aplicação.

---
+++ src/main/java/com/mateuslll/taskflow/TaskflowManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mateuslll.taskflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowManagerApplication.class, args);
+	}
+
+}
